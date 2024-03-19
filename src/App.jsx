d60e6305f62de92cf8af1ecb823b393d9601b72e
Tejpex <equis_angel@hotmail.com>
2024-03-19 14:05:07 +0100

    Renders dog facts
    Co-authored-by: fridaforser <fridaforser@users.noreply.github.com>
    Co-authored-by: Lovisa Åberg <Lovisaaberg@users.noreply.github.com>
    Co-authored-by: Pernilla Sterner <hey@pernillasterner.se>
    Co-authored-by: AHPIXI <AHPIXI@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 // Import required  hooks
+import { useState, useEffect } from "react";
 import { DogFact } from "./components/DogFact";
 
 export const App = () => {
+  const [dogfacts, setDogfacts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dogapi.dog/api/v2/facts")
+      .then((res) => res.json())
+      .then((json) => {
+        setDogfacts(json.data[0].attributes);
+        console.log(json);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log("Dog", dogfacts);
+  },[dogfacts])
+
   // Hint: Initialize state for storing the dog fact
-  // Hint: Define the API endpoint
+  // Hint: Define the API endpoint https://dogapi.dog/api/v2/facts
   // Hint: Create a function to fetch the dog fact
   // Hint: Use the useEffect hook to fetch the dog fact when the component mounts
 
   return (
     <div className="App">
-      <DogFact />
+      <DogFact dogfact={dogfacts}/>
     </div>
   );
 };
